4c41cf3f767a60b2b98df4abb8b6cf767bc85c15
yarocruz <yarocruz@gmail.com>
2023-12-20 08:28:39 -0500

    add solved day8 part1

--- 2023/rust/day8/src/bin/part1.rs
+++ 2023/rust/day8/src/bin/part1.rs
@@ -14,22 +14,9 @@ fn main() {
 we want to create a linked list of nodes
 */
 
-type NodeLink = Option<Rc<RefCell<Node>>>;
-
 struct Node {
-    pub value: String,
-    pub left: NodeLink,
-    pub right: NodeLink,
-}
-
-impl Node {
-    fn new(value: String) -> Rc<RefCell<Self>> {
-        Rc::new(RefCell::new(Node {
-            value,
-            left: None,
-            right: None,
-        }))
-    }
+    value: String,
+    children: Vec<Node>,
 }
 
 fn part1(input: &str) -> String {
@@ -40,75 +27,53 @@ fn part1(input: &str) -> String {
         .next()
         .unwrap();
 
-    // after the empty line, we have the following format:
-    // AAA = (BBB, CCC)
-    // BBB = (DDD, EEE)
-    // each of these can be represented as node with left and right children
-    // create linked list of nodes
+    // for every line in the input that starts with AAA = (BBB, CCC)
+    // create a node with value AAA
+    // add BBB and CCC as children
 
-    let tree = build_tree(&input.lines().skip(2).collect::<Vec<&str>>());
-    let mut count = 0;
+    let mut nodes = input
+        .lines()
+        .skip(2)
+        .map(|line| {
+            let mut parts = line.split(" = ");
+            let value = parts.next().unwrap();
+            let children = parts.next().unwrap();
+            let children = children
+                .trim_start_matches("(")
+                .trim_end_matches(")")
+                .split(", ")
+                .map(|child| child.to_string())
+                .collect::<Vec<String>>();
+            (value.to_string(), children)
+        })
+        .collect::<HashMap<String, Vec<String>>>();
 
-    // Example of accessing a node and its children
-    if let Some(node_link_option) = tree.get("AAA") {
-        if let Some(node_rc) = node_link_option {
-            let node_borrow: Ref<Node> = node_rc.as_ref().borrow();
-            while node_borrow.value != "ZZZ" {
+        let mut current = "AAA";
+        let mut count = 0;
+        let max_iterations = 100000; // Prevents infinite loop
+        
+        while current != "ZZZ" && count < max_iterations {
+            for direction in directions.chars() {
+                println!("direction: {}", direction);
+                let next_node = match direction {
+                    'L' => &nodes.get(current).unwrap()[0],
+                    'R' => &nodes.get(current).unwrap()[1],
+                    _ => panic!("Invalid direction"),
+                };
+        
+                current = next_node;
                 count += 1;
-                for direction in directions.chars() {
-                    match direction {
-                        'L' => {
-                            if let Some(left_node) = node_borrow.left {
-                                if let Some(left) = left_node.as_ref().borrow().left {
-                                    node_borrow = left.as_ref().borrow();
-                                }
-                                
-                            }
-                        }
-                        'R' => {
-                            if let Some(right_node) = node_borrow.right {
-                                if let Some(right) = right_node.as_ref().borrow().right {
-                                    node_borrow = right.as_ref().borrow();
-                                }
-                            }
-                        }
-                        _ => panic!("Unknown direction"),
-                    }
-                }
             }
-            println!("AAA: {}", node_borrow.value);
+        
         }
-    }
+        
+        if count >= max_iterations {
+            println!("Maximum iterations reached, terminating");
+        }
 
     println!("count: {}", count);
 
-    "result".to_string()
-}
-
-fn build_tree(input: &[&str]) -> HashMap<String, NodeLink> {
-    let mut nodes = HashMap::new();
-
-    for line in input {
-        let parts: Vec<&str> = line.split(" = (").collect();
-        let value = parts[0].to_string();
-        let children: Vec<&str> = parts[1].trim_end_matches(')').split(", ").collect();
-
-        // Ensure that nodes are inserted as Some(Rc<RefCell<Node>>)
-        let left_node = nodes.entry(children[0].to_string())
-                            .or_insert_with(|| Some(Node::new(children[0].to_string())))
-                            .clone();
-        let right_node = nodes.entry(children[1].to_string())
-                             .or_insert_with(|| Some(Node::new(children[1].to_string())))
-                             .clone();
-
-        let parent_node = Node::new(value);
-        parent_node.borrow_mut().left = left_node;
-        parent_node.borrow_mut().right = right_node;
-
-        nodes.insert(parts[0].to_string(), Some(Rc::clone(&parent_node)));
-    }
-
-    nodes
+    count.to_string()
 }
 
 #[cfg(test)]
---
+++ 2023/rust/day8/target/debug/deps/part1-f6a4688e77f12d3d.d
@@ -0,0 +1,5 @@
+/Users/yaro/Desktop/dev/advent-of-code/2023/rust/day8/target/debug/deps/part1-f6a4688e77f12d3d: src/bin/part1.rs
+
+/Users/yaro/Desktop/dev/advent-of-code/2023/rust/day8/target/debug/deps/part1-f6a4688e77f12d3d.d: src/bin/part1.rs
+
+src/bin/part1.rs:
