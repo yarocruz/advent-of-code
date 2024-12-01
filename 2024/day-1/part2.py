9aac873d749d53c9a55492f01db623d6feafe68c
yarocruz <yarocruz@gmail.com>
2024-12-01 08:58:01 -0500

    solved day1 part2

---
+++ 2024/day-1/part2.py
@@ -0,0 +1,24 @@
+def solve(input_srt):
+    lines = input_srt.strip().split("\n")
+    score = []
+
+    left_list = []
+    right_list = []
+
+    for line in lines:
+        # split the line by space in between
+        temp = line.split("   ")
+        left_list.append(int(temp[0]))
+        right_list.append(int(temp[1]))
+
+    # let's do brute force here because can think of 
+    # a way to not do double loop
+    for i in range(len(left_list)):
+        count = 0
+        for j in range(len(right_list)):
+            if right_list[j] == left_list[i]:
+                count += 1
+        score.append(left_list[i] * count)
+    
+    result = sum(score)
+    return str(result)
---
+++ 2024/day-1/test_part2.py
@@ -0,0 +1,16 @@
+import unittest
+from part2 import solve
+
+class TestPart1(unittest.TestCase):
+    def test_sample_input(self):
+        INPUT = """3   4
+4   3
+2   5
+1   3
+3   9
+3   3"""
+        result = solve(INPUT)
+        self.assertEqual(result, "31")
+
+if __name__ == "__main__":
+    unittest.main()
