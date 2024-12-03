1d9b6e82df64a5c721652afbcba3a0ec966473d0
yarocruz <yarocruz@gmail.com>
2024-12-02 21:32:10 -0500

    solve day 2 part 2

--- 2024/day-2/part2.py
+++ 2024/day-2/part2.py
@@ -1,61 +1,32 @@
 def solve(input_srt):
     lines = input_srt.strip().split("\n")
-    
-    reports = []
-    unsafe_count = 0
-    unsafe_tolerance = 0
-    
-    for line in lines:
-        report = line.split(" ")
-        reports.append(report)
+    reports = [[int(num) for num in line.split()] for line in lines]
+    safe_count = 0
 
     for report in reports:
-        decreasing = is_valid_decrease(int(report[0]), int(report[1]))
-        increasing = is_valid_increase(int(report[0]), int(report[1]))
-        
-        for i in range(1, len(report)):
-            if increasing and i < len(report) - 1:
-                if not is_valid_increase(int(report[i]), int(report[i+1])):
-                    unsafe_tolerance += 1
-                    
-                    if unsafe_tolerance > 1:
-                        
-                        unsafe_count += 1
-                        unsafe_tolerance = 0
-                        break
+        if is_safe(report):
+            safe_count += 1
+        else:
+            for i in range(len(report)):
+                modified_report = report[:i] + report[i+1:]
+                if is_safe(modified_report):
+                    print(report)
+                    break 
 
-                decreasing = is_valid_decrease(int(report[i]), int(report[i+1]))
-                    
-            if decreasing and i < len(report) - 1:
-                if not is_valid_decrease(int(report[i]), int(report[i+1])):
-                    unsafe_tolerance += 1
-                    
-                    if unsafe_tolerance > 1:
-                        
-                        unsafe_count += 1
-                        unsafe_tolerance = 0
-                        break
-                increasing = is_valid_increase(int(report[i]), int(report[i+1]))  
+    return str(safe_count)
 
-            if not decreasing and not increasing:
-                unsafe_tolerance += 1
-                increasing = is_valid_increase(int(report[i]), int(report[i+1]))
-                decreasing = is_valid_decrease(int(report[i]), int(report[i+1]))
-                if unsafe_tolerance > 1:
-                        unsafe_count += 1
-                        unsafe_tolerance = 0
-                        break
 
-    print(len(reports), unsafe_count)       
-
-    return str(len(reports) - unsafe_count)
-
-def is_valid_increase(curr, next):
-    if next - curr > 0 and next - curr <= 3:
+def is_safe(report):
+    diffs = []
+    for i in range(len(report) - 1):
+        diff = report[i + 1] - report[i]
+        if diff == 0:
+            return False
+        diffs.append(diff)
+    
+    if all(1 <= d <= 3 for d in diffs):
         return True
-    return False
-
-def is_valid_decrease(curr, next):
-    if curr - next > 0 and curr - next <= 3:
+    elif all(-3 <= d <= -1 for d in diffs):
         return True
-    return False
+    else:
+        return False
