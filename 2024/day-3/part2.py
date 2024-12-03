8c7cc505cb70571b7a97a71f9b83fd7b1c545277
yarocruz <yarocruz@gmail.com>
2024-12-03 17:41:25 -0500

    add solved day3 part2

--- 2024/day-3/part2.py
+++ 2024/day-3/part2.py
@@ -1,4 +1,24 @@
-def solve(input_srt):
-    lines = input_srt.strip().split("\n")      
+import re
 
-    return str("")
+def solve(input_srt):
+    # this one we include the do(), don't() and mul(x,y) in the pattern
+    pattern = r'do\(\)|don\'t\(\)|mul\(\d+,\d+\)'
+    numbers = r"\d{1,3}"
+
+    result = 0
+    enabled = True
+
+    tokens = re.findall(pattern, input_srt)
+
+    # we create a queue like system base on the enable bool
+    for token in tokens:
+        if token == 'do()':
+            enabled = True
+        elif token == 'don\'t()':
+            enabled = False
+        elif token.startswith('mul(') and enabled:
+            nums = re.findall(numbers, token)
+            x, y = nums
+            result += int(x) * int(y)
+
+    return str(result)
--- 2024/day-3/test_part2.py
+++ 2024/day-3/test_part2.py
@@ -3,10 +3,9 @@ from part2 import solve
 
 class TestPart1(unittest.TestCase):
     def test_sample_input(self):
-        INPUT = """
-"""
+        INPUT = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
         result = solve(INPUT)
-        self.assertEqual(result, "")
+        self.assertEqual(result, "48")
 
 if __name__ == "__main__":
     unittest.main()
