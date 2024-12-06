4c4f7e24ace85ce39959ef7622e9e767dfbfc6ba
yarocruz <yarocruz@gmail.com>
2024-12-05 19:31:26 -0500

    solved day-5 part1

--- 2024/day-5/part1.py
+++ 2024/day-5/part1.py
@@ -1,6 +1,30 @@
 import numpy as np
 
 def solve(input_srt):
-    lines = input_srt.strip().split("\n")
+    rules, print_order = input_srt.strip().split("\n\n")
+    rules = [rule.split("|") for rule in rules.split("\n")]
+    print_orders = [order.split(",") for order in print_order.split("\n")]
 
-    return str("")
+    valid_orders = []
+    for update in print_orders:
+        # Map page number to indices
+        pos = {page: idx for idx, page in enumerate(update)}
+
+        correct = True
+        for l, r in rules:
+            if l in pos and r in pos:
+                if pos[l] > pos[r]:
+                    correct = False
+                    break
+        
+        if correct:
+            valid_orders.append(update)
+
+    # loop through valid orders, find the middle, assuming that each
+    # update will be and odd number length
+    result = 0
+    for order in valid_orders:
+        mid_idx = len(order) // 2
+        result += int(order[mid_idx])
+
+    return str(result)
