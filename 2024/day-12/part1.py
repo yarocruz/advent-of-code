ea7c5f250dd910b88c2c6ef73b6ed9e36197ecf4
yarocruz <yarocruz@gmail.com>
2024-12-12 08:03:08 -0500

    setup day-12

---
+++ 2024/day-12/part1.py
@@ -0,0 +1,8 @@
+def solve(input_srt):
+  initial_stone_arrangement = input_srt.strip().split(" ")
+
+    
+  return str("")
+
+
+  
---
+++ 2024/day-12/part2.py
@@ -0,0 +1,6 @@
+from collections import Counter
+
+def solve(input_srt):
+  initial_stone_arrangement = input_srt.strip().split(" ")
+  
+  return str("")
