b0708393b530bb6b598253cae5c1bff243e6c628
yarocruz <yarocruz@gmail.com>
2024-12-13 19:30:22 -0500

    setup day13

---
+++ 2024/day-13/part1.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def solve(input_srt):
+  lines = input_srt.strip().split("\n")
+  grid = np.array([[row for row in line.strip()] for line in lines])
+
+  return str("")
+
+
+
---
+++ 2024/day-13/test_part2.py
@@ -0,0 +1,25 @@
+import unittest
+from part1 import solve
+
+class TestPart1(unittest.TestCase):
+    def test_sample_input(self):
+        INPUT = """Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279"""
+        result = solve(INPUT)
+        self.assertEqual(result, "480")
+
+if __name__ == "__main__":
+    unittest.main()
